13ab23d0f6d05395d5f7febb84bb264125784425
Jahedul-Hoque <Jahedul4321@gmail.com>
2025-02-08 14:56:57 +0000

    Adding to my Mathematical basic expressions

--- Intro to C++/Basic Mathematical Expressions/Basic Mathematical Expressions.cpp
+++ Intro to C++/Basic Mathematical Expressions/Basic Mathematical Expressions.cpp
@@ -2,6 +2,16 @@
 
 int main()
 {
-    std::cout << "Hello World!";
-   
+	int x = 10;
+	int y = x++; // x will be assigned to y - and then will be incremented by 1
+	int c = x;
+
+	
+	std::cout << c; //c will be given the value of 11 and y will be given the value of 10
+
+	int z = ++c; //z will be given the value of 12 as the value of c was incrememented before being assigned to z
+
+	double j = 1 + 2 * 3; // following bidmas, j will be given the value of 7
+	std::cout << j;
+	
 }
