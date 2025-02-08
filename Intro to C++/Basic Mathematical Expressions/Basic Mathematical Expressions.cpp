a2cfb6e3af20abca80a4ea62622a9f3f7d07f9f9
Jahedul-Hoque <Jahedul4321@gmail.com>
2025-02-08 15:01:11 +0000

    cleaned up code

--- Intro to C++/Basic Mathematical Expressions/Basic Mathematical Expressions.cpp
+++ Intro to C++/Basic Mathematical Expressions/Basic Mathematical Expressions.cpp
@@ -6,12 +6,13 @@ int main()
 	int y = x++; // x will be assigned to y - and then will be incremented by 1
 	int c = x;
 
-	
-	std::cout << c; //c will be given the value of 11 and y will be given the value of 10
+
+	std::cout << "C is equal to " << c << std::endl; //c will be given the value of 11 and y will be given the value of 10
 
 	int z = ++c; //z will be given the value of 12 as the value of c was incrememented before being assigned to z
 
 	double j = 1 + 2 * 3; // following bidmas, j will be given the value of 7
-	std::cout << j;
+	std::cout << "j is equal to " << j << std::endl;
+	return 0;
 	
 }
