312cc5bfb03ca6d026e6b3b35a01f9cec0a29388
Jahedul-Hoque <Jahedul4321@gmail.com>
2025-02-08 23:32:01 +0000

    Working on Functions and Declarations

--- Intro to C++/03 Functions and Declarations/03 Functions and Declarations.cpp
+++ Intro to C++/03 Functions and Declarations/03 Functions and Declarations.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!";
-    cout << endl;
+
+void LoadTradingData(int TradeID, string TradeSymbol) {
+	//function initialised
+	cout << "Trade ID: " << TradeID << "\n";
+	cout << "Trade Symbol: " << TradeSymbol << "\n";
+	//passes in a variable called TradeID as a parameter thats declared in "main" function.
+	cout << "Trade Security: Gold\n";
+	cout << "Trade Date: 12:05:2024\n";
+	cout << "Trade Quantity: 234\n";
+	cout << "Trade Price: 31235\n";
 }
+int main(){
+
+	//function = a block of reusable code
+	int TradeID = 1235;
+	string TradeSymbol = "Schonfeld Securities";
+	//declares a variable that will be used to pass in as a parameter in the "LoadTradingData" function
+	LoadTradingData(TradeID, TradeSymbol);
+	//invokes the function
+
+
+}
+
+
