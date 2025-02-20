8f3375d322dfd6cf57096186f6af48fd6738c6de
Jahedul-Hoque <Jahedul4321@gmail.com>
2025-02-20 22:33:33 +0000

    adding to .cpp

--- Atomic Operations/Atomic Operations.cpp
+++ Atomic Operations/Atomic Operations.cpp
@@ -7,19 +7,24 @@ int main()
 {
 	int counter = 0;
 	auto work = [&counter] {
+	// work will be a random data type and will use the reference counter
 		for (int i = 0;i < 100; i++)
 			counter++;
 		};
 
-	std::thread t1(work);
 
+	std::thread t1(work);
 	std::thread t2(work);
+	// using 2 threads to increment counter at the same time
+
 
 	t1.join();
 	t2.join();
+	// joining both threads together
 
 
 	std::cout << counter << "\n";
 	return 0;
+	///
 }
 
