acf394611a5acaf1a95c958063cf66dfdf2c945f
Jahedul-Hoque <Jahedul4321@gmail.com>
2025-02-22 13:41:07 +0000

    adding lock guard and unique lock in the code

--- Mutex & Lock Guard/Mutex & Lock Guard.cpp
+++ Mutex & Lock Guard/Mutex & Lock Guard.cpp
@@ -11,13 +11,17 @@ mutex Lock;
 
 void Trade(std::string TraderName) {
     std::lock_guard<mutex> TradeLock(Lock);
-    // making a local Mutex Object
+    // making a local lockguard object named TradeLock
+    // std::unique_lock<mutex> TradeLock(Lock);
+    // ^^ used for unique locks
+   
     std::cout << TraderName << " is setting up the systematic trading applications\n";
     std::this_thread::sleep_for(chrono::seconds(1));
     std::cout << TraderName << " is done setting up the algorithms. \n";
     std::this_thread::sleep_for(chrono::seconds(2));
     
-    // the TradeLock auto unlocks when Lock goes out of scope
+    // the lock guard auto unlocks when Lock goes out of scope
+    // unique lock means you NEED to manually unlock the code
    
 }
 
